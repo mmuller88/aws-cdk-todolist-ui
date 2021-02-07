333ee27f369ffde6e9a73ed090ee3c79001d1cae
Martin Mueller <damadden88@googlemail.com>
2021-02-07 14:21:05 +0100

    feat: prepare amplify components

--- .projenrc.js
+++ .projenrc.js
@@ -6,7 +6,66 @@ const project = new web.ReactTypeScriptProject({
   defaultReleaseBranch: 'main',
   jsiiFqn: "projen.web.ReactTypeScriptProject",
   name: 'aws-cdk-todolist-ui',
+  deps: [
+    '@aws-amplify/auth',
+    '@aws-amplify/ui-components',
+    '@aws-amplify/ui-react',
+    'aws-amplify',
+    'react-query@^2', // I have an open PR for react-query v3 support
+    'react-router',
+    'react-router-dom',
+  ],
+  devDeps: [
+    '@graphql-codegen/cli',
+    '@graphql-codegen/typescript',
+    '@graphql-codegen/typescript-operations',
+    '@graphql-codegen/typescript-react-query@alpha',
+    'amplify-graphql-docs-generator',
+    'aws-sdk@^2',
+    'graphql',
+  ],
 
+  gitignore: [
+    'aws-exports.js',
+  ],
+
+  tsconfig: {
+    compilerOptions: {
+      allowJs: true,
+      skipLibCheck: true,
+      esModuleInterop: true,
+      allowSyntheticDefaultImports: true,
+      forceConsistentCasingInFileNames: false,
+      module: 'esnext',
+      moduleResolution: 'node',
+      isolatedModules: true,
+      noEmit: true,
+      jsx: 'react-jsx',
+    },
+  },
+});
+
+project.addTask('dev', {
+  description: 'Runs the application locally',
+  exec: 'react-scripts start',
+});
+
+project.addTask('generate-exports', {
+  description: 'Generates aws-exports.js',
+  exec: 'node bin/generateExports.js',
+});
+
+project.addTask('copy-schema', {
+  exec: 'cp ../appsync/schema.graphql ./schema.graphql',
+});
+
+project.addTask('generate-statements', {
+  exec: 'node bin/generateStatements.js',
+});
+
+project.addTask('codegen', {
+  description: 'Copies the backend schema and generates frontend code',
+  exec: 'yarn run copy-schema && yarn run generate-statements && graphql-codegen --config codegen.yml && rm schema.graphql',
 });
 
 project.synth();
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# replace this
+# aws-cdk-todolist-ui
+
+AWS Amplify frontend for the appsync backend [aws-cdk-todolist](https://github.com/mmuller88/aws-cdk-todolist-ui)
+
+# Thanks to:
+* kcwinner for this very inspiring TS React Amplify project https://github.com/kcwinner/cdk-appsync-react-demo
