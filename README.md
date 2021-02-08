2d2bb134c264d5640296617a28c2fbc7fec5e0ec
Martin Mueller <damadden88@googlemail.com>
2021-02-08 07:43:13 +0100

    feat: prepare amplify components

--- README.md
+++ README.md
@@ -2,5 +2,23 @@
 
 AWS Amplify frontend for the appsync backend [aws-cdk-todolist](https://github.com/mmuller88/aws-cdk-todolist-ui)
 
+# Deploy
+1. Test And Deploy
+```bash
+yarn test
+yarn deploy
+```
+
+1. Generate Exports && GraphQL Types
+```
+yarn generate-exports
+yarn codegen
+```
+
+1. Run the frontend locally
+```bash
+yarn dev
+```
+
 # Thanks to:
 * kcwinner for this very inspiring TS React Amplify project https://github.com/kcwinner/cdk-appsync-react-demo
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css'
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+import { API } from './lib/fetcher';
+
+import { Posts } from './components/posts';
+import { Todos } from './components/todos';
+
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+
+Amplify.configure(config);
 
 function App() {
+
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData: any) => {
+      API.updateIsSignedIn(nextAuthState === AuthState.SignedIn);
+    });
+  }, []);
+
   return (
-   <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+      <nav className="Navbar">
+        <h1 className="navbar-logo">AppSync Transformer Demo</h1>
+        <ul className="nav-menu">
+          <li> <a href="/posts">Posts</a></li>
+          <li> <a href="/todos">Todos</a></li>
+        </ul>
+        <AmplifySignOut />
+      </nav>
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" render={(props: any) => <Posts {...props} />} />
+            <Route path="/posts" render={(props: any) => <Posts {...props} />} />
+            <Route path="/todos" render={(props: any) => <Todos {...props} />} />
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
