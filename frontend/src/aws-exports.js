8b67b3e982cb416ce208ef7af6dd130ba1c087f7
Martin Mueller <damadden88@googlemail.com>
2021-02-12 15:29:49 +0100

    feat: add react build command to pipe

---
+++ frontend/src/aws-exports.js
@@ -0,0 +1,12 @@
+const awsmobile = {
+    "aws_project_region": "eu-central-1",
+    "aws_appsync_graphqlEndpoint": "https://msw55sq645esjkf57oouxxnwom.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_region": "eu-central-1",
+    "aws_appsync_authenticationType": "AWS_IAM",
+    "aws_cognito_region": "eu-central-1",
+    "aws_user_pools_id": "eu-central-1_84KRaoqOo",
+    "aws_user_pools_web_client_id": "4c3407a1lda87eng5t4oselab5",
+    "aws_cognito_identity_pool_id": "eu-central-1:4a7687f1-4138-4212-addc-43573d112483"
+}
+    
+export default awsmobile;
