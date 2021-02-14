4b04755517baf2754e2a0662d626e04a22ece6da
Martin Mueller <damadden88@googlemail.com>
2021-02-14 15:17:51 +0100

    feat: add stagename to logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,7 +27,7 @@ function App() {
   return (
     <div className="App">
       <nav className="Navbar">
-        <h1 className="navbar-logo">AppSync Transformer Demo</h1>
+        <h1 className="navbar-logo">AppSync Transformer Demo - {window.ENV.stage || 'no'} Stage</h1>
         <ul className="nav-menu">
           <li> <a href="/todos">Todos</a></li>
         </ul>
