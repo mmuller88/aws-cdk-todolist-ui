89feb58e6ba57392425169fd56271c5dac2c4447
Martin Mueller <damadden88@googlemail.com>
2021-04-05 15:40:08 +0200

    add todolist component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,27 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css'
 
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
+import { API } from './lib/fetcher';
+
 import Amplify from 'aws-amplify';
+
+import { Todos } from './components/todos';
 
 declare const window: any;
 
 Amplify.configure(window.ENV);
 
 function App() {
+
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData: any) => {
+      API.updateIsSignedIn(nextAuthState === AuthState.SignedIn);
+    });
+  }, []);
 
   return (
     <div className="App">
@@ -23,8 +35,8 @@ function App() {
     <div>
       <Router>
         <Switch>
-          <Route exact path="/" render={(props: any) => <div {...props}>root</div>} />
-          <Route path="/todos" render={(props: any) => <div {...props}>todos</div>} />
+          <Route exact path="/" render={(props: any) => <Todos {...props}/>} />
+          <Route path="/todos" render={(props: any) => <Todos {...props}/>} />
         </Switch>
       </Router>
     </div>
