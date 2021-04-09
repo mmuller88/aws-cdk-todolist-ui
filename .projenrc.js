c90c14a0109d3668a6f7d5d5b3da96e7d1916a0e
Martin Mueller <damadden88@googlemail.com>
2021-04-09 19:15:59 +0200

    remove s3 cdk stack and pipe

--- .projenrc.js
+++ .projenrc.js
@@ -6,30 +6,7 @@ const project = new AwsCdkTypeScriptApp({
   name: 'aws-cdk-todolist-ui',
   defaultReleaseBranch: 'main',
   cdkVersion: '1.93.0',
-  cdkDependencies: [
-    '@aws-cdk/aws-iam',
-    '@aws-cdk/aws-s3-deployment',
-    '@aws-cdk/aws-s3',
-    '@aws-cdk/aws-codepipeline',
-    '@aws-cdk/aws-codepipeline-actions',
-    '@aws-cdk/pipelines',
-    '@aws-cdk/aws-lambda-nodejs',
-  ],
-  deps: [
-    'aws-cdk-staging-pipeline',
-  ],
-  context: {
-    '@aws-cdk/core:enableStackNameDuplicates': true,
-    'aws-cdk:enableDiffNoFail': true,
-    '@aws-cdk/core:stackRelativeExports': true,
-    '@aws-cdk/core:newStyleStackSynthesis': true,
-  },
-  releaseWorkflow: false,
+  cdkVersionPinning: true,
 });
-
-project.buildTask.prependExec('cd frontend && yarn install && yarn build && cd ..');
-
-project.setScript('cdkDeploy', 'cdk deploy');
-project.setScript('cdkDestroy', 'cdk destroy');
 
 project.synth();
